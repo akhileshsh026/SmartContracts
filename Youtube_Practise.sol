ec9122cb3050ef75c2492eed04265e5889a891ab
akhileshsh026 <akhileshsh026@hotmail.com>
2019-07-10 08:33:38 +0530

    Calculator using the uint

--- Youtube_Practise.sol
+++ Youtube_Practise.sol
@@ -1,8 +1,46 @@
-pragma solidity ^0.5.1;
+pragma solidity ^0.5.0;
 
-contract demo 
+contract Calculator  
 {
-     bytes a = 12;
-     bytes b = 13;
+         uint a = 12;
+         uint b = 13;
+         
+                    uint add_result = add(a,b);
+                    uint minus_result = minus(a,b);
+                    uint mul_result = mul(a,b);
+                    uint divide_result = divide(a,b);
+     
+   
+                    
+                    
+                    function add(uint c,uint d) public pure returns(uint) {
+                        return c+d;
+                    }
+                    
+                                        function minus(uint c,uint d) public pure returns(uint) {
+                                               if(c>d)
+                                               {
+                                                   return c-d;
+                                               } else
+                                               {
+                                                   return d-c;
+                                               }
+                    }
+                    
+                                        function mul(uint c,uint d) public pure returns(uint) {
+                        return c*d;
+                    }
+                    
+                                        function divide(uint c,uint d) public pure returns(uint) {
+                                                 if(c>d)
+                                               {
+                                                   return c/d;
+                                               } else
+                                               {
+                                                   return d/c;
+                                               }
+                    }
+
+   
 
 }
