e11496eb781206e6a944ed3f205e2acd8f9defd9
akhileshsh026 <akhileshsh026@hotmail.com>
2019-07-10 09:06:31 +0530

    Enums in actions with default state

--- Youtube_Practise.sol
+++ Youtube_Practise.sol
@@ -1,46 +1,12 @@
 pragma solidity ^0.5.0;
 
-contract Calculator  
+contract Enums  
 {
-         uint a = 12;
-         uint b = 13;
-         
-                    uint add_result = add(a,b);
-                    uint minus_result = minus(a,b);
-                    uint mul_result = mul(a,b);
-                    uint divide_result = divide(a,b);
-     
-   
-                    
-                    
-                    function add(uint c,uint d) public pure returns(uint) {
-                        return c+d;
-                    }
-                    
-                                        function minus(uint c,uint d) public pure returns(uint) {
-                                               if(c>d)
-                                               {
-                                                   return c-d;
-                                               } else
-                                               {
-                                                   return d-c;
-                                               }
-                    }
-                    
-                                        function mul(uint c,uint d) public pure returns(uint) {
-                        return c*d;
-                    }
-                    
-                                        function divide(uint c,uint d) public pure returns(uint) {
-                                                 if(c>d)
-                                               {
-                                                   return c/d;
-                                               } else
-                                               {
-                                                   return d/c;
-                                               }
-                    }
+        enum Game {
+             keepPlaying,
+             pause,
+             disucnnected,reconnecting
+              }
 
-   
-
+              Game constant defaultState = Game.keepPlaying ;
 }
