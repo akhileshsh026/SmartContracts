f8fca45f4f9a8eb516cc7d8f043dd94249f936ad
akhileshsh026 <akhileshsh026@hotmail.com>
2019-07-13 16:44:43 +0530

    Struct mapped with the help of mapping

--- Dapp_univecity.sol
+++ Dapp_univecity.sol
@@ -5,17 +5,19 @@ contract DappUnivercity
 {
      
     struct Person {
+        uint256 pid;
         string _firstName;
         string _last_Name;
     }
     
-    Person[] public people;
+    mapping(uint => Person)public people;
+    
     uint256 public PeopleCount;
     
     function addPerson(string memory _fn,string memory _ln) public 
     {
-               people.push(Person(_fn,_ln));
-               PeopleCount +=1;
+        PeopleCount +=1;
+        people[PeopleCount] = Person(PeopleCount,_fn,_ln);
     }
 
 }
