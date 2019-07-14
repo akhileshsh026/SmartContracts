3dabc3320ab9cfeadf26dce7bc1eb6b8949c1360
akhileshsh026 <akhileshsh026@hotmail.com>
2019-07-14 08:40:45 +0530

    paying hte ethers

--- Dapp_univecity.sol
+++ Dapp_univecity.sol
@@ -3,42 +3,19 @@ pragma solidity ^0.5.0;
 
 contract DappUnivercity 
 {
-        struct Person {
-     //   uint256 pid;
-        string _firstName;
-        string _last_Name;
-    }
-    
-    mapping(uint => Person)public people;
-    
-    address owner;
-    
-    uint256 public PeopleCount;
-    
-    uint256 opening_time = 1563018969; 
-    
-    modifier OnlyOwner() {
-        require(msg.sender == owner,"You must be an Owner");
-        _;
-    }
-    
-    modifier OnlyinTime() {
-        require(block.timestamp >= opening_time);
-        _;
-    }
-    
-    constructor() public {
-        owner = msg.sender;
-    }
-    
-    function addPerson(string memory _fn,string memory _ln) public OnlyOwner OnlyinTime
-    {
-        IncrementCount();
-        people[PeopleCount] = Person(_fn,_ln);
-    }
-    
-    function IncrementCount() internal {
-        PeopleCount +=1;
-    }
+
+ mapping(address => uint256) public balances;
+ address payable wallet;
+ 
+ 
+ constructor(address payable _wallet) public
+ {
+     wallet = _wallet;
+ }
+ 
+ function BuyTokens() public payable {
+     balances[msg.sender] += 1;
+     wallet.transfer(msg.value);
+ }
 
 }
